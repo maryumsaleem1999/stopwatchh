a047ea4e0f3b32b57ebe6f42df2fa76b97136104
maryumsaleem <maryumangel3@gmail.com>
2020-07-11 21:48:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var min=0;
+var sec=0;
+var msec=0;
+var minute=document.getElementById('min');
+var second=document.getElementById('sec');
+var msecond=document.getElementById('msec');
+var interval;
+function timer(){
+	msec++
+	msecond.innerHTML=msec;
+	if(msec>=100){
+		sec++
+		second.innerHTML=sec;
+		msec=0;
+	}else if(sec>=60){
+		min++
+		sec=0
+		minute.innerHTML=min
+	}
+}
+var status="play"
+function play(){
+	interval=setInterval(timer,10)
+   document.getElementById("play").disabled=true; 
+}
+function stop(){
+	clearInterval(interval) 
+	   document.getElementById("play").disabled=true; 
+ 
+}
+function reset(){
+ min=0;
+ sec=0;
+ msec=0;
+  minute.innerHTML=min
+ second.innerHTML=sec
+msecond.innerHTML=msec
+	   document.getElementById("play").disabled=false; 
+
+}
